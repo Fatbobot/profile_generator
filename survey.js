cb9df695c3ae2b42179c32e986e289d2e16d20da
Paul Jean <Fatbobot@gmail.com>
2023-11-11 06:57:57 -0400

    made changes to reference errors

--- survey.js
+++ survey.js
@@ -1,5 +1,3 @@
-const readline = require('readline');
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -8,29 +6,21 @@ const rl = readline.createInterface({
 rl.question(`What's your name Nicknames are also acceptable :) `, (answer) => {
   console.log(`Thank you for your valuable feedback: ${answer}`);
 
-  rl.close();
-});
+  rl.question(`What's an activity you like doing? `, (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
 
-r2.question(`What's and activity you like doing?`, (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+    rl.question('What do you listen to while doing that? ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
 
-  r2.close();
-});
+      rl.question('Which meal is your favourite (e.g., dinner, brunch, etc.) ', (answer) => {
+        console.log(`Thank you for your valuable feedback: ${answer}`);
 
-r3.question('What do you listen to while doing that? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+        rl.question(`What's your favourite thing to eat for that meal? `, (answer) => {
+          console.log(`Thank you for your valuable feedback: ${answer}`);
 
-  r3.close();
-});
-
-r4.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  r4.close();
-});
-
-r5.question(`What's your favourite thing to eat for that meal? `, (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  r5.close();
+          rl.close();
+        });
+      });
+    });
+  });
 });
